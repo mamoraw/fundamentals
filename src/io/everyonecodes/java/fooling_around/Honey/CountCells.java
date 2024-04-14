52a1934452384c474a2d964973abaa95f6a5c608
mamoraw <144896087+mamoraw@users.noreply.github.com>
2024-04-14 21:21:47 +0200

    Honeycomb exe2

--- src/io/everyonecodes/java/fooling_around/Honey/CountCells.java
+++ src/io/everyonecodes/java/fooling_around/Honey/CountCells.java
@@ -9,74 +9,18 @@ import java.util.List;
 
 public class CountCells {
     public static void main(String[] args) throws IOException {
-        String honey = """
-10
 
-O-X-X-O-O-X-O-O-O-O-X-O-O-O-X-O-
--O-X-O-O-O-W-O-O-X-X-X-O-O-X-X-X
-O-O-O-X-O-X-O-O-O-X-X-X-X-O-O-X-
--X-X-O-O-O-X-X-X-O-X-O-O-X-O-O-O
-
-O-X-O-O-O-X-X-X-O-O-X-O-X-X-X-X-
--O-O-X-O-O-X-O-X-O-X-X-O-O-O-X-X
-X-O-O-X-X-O-O-X-O-X-O-X-W-X-O-X-
--O-O-X-O-O-O-O-O-O-O-O-O-O-X-O-O
-
-X-X-X-O-O-O-X-X-X-O-O-O-O-O-X-O-
--O-O-O-O-O-O-O-O-X-O-O-X-O-O-O-X
-X-O-O-X-O-X-X-O-X-O-O-W-O-O-O-O-
--O-O-O-O-O-O-X-O-O-O-X-O-O-X-X-O
-
-X-O-X-X-O-O-O-X-X-X-X-X-X-O-X-X-
--O-O-X-X-O-O-O-O-O-O-O-X-O-X-O-O
-O-X-O-O-X-O-W-X-O-O-X-O-O-O-O-O-
--X-X-O-O-X-O-O-O-X-X-X-O-X-X-O-X
-
-O-X-O-X-X-X-O-O-O-O-O-O-X-O-X-O-
--O-O-X-O-O-X-O-O-X-X-W-X-O-X-X-X
-O-O-O-O-O-O-O-O-O-O-O-X-O-O-O-X-
--O-X-X-O-X-X-O-O-O-O-O-O-X-O-X-O
-
-X-O-X-X-O-X-X-X-O-O-O-O-X-O-X-X-
--X-X-O-W-O-X-X-X-O-X-X-O-X-X-O-X
-O-O-O-X-O-O-O-O-X-O-X-X-X-O-X-O-
--O-X-X-X-X-X-X-X-X-O-X-O-O-O-X-X
-
-X-O-O-O-O-X-X-O-O-O-O-O-O-O-X-O-
--O-O-X-O-O-X-O-X-O-O-X-O-O-O-O-X
-X-X-X-O-O-O-X-O-O-O-W-O-X-X-X-O-
--X-X-X-X-O-O-X-O-X-X-X-O-X-O-X-O
-
-O-O-X-O-O-X-O-O-O-O-X-O-O-X-O-X-
--O-X-X-X-O-O-O-O-X-O-O-O-O-O-O-X
-O-X-O-O-O-O-X-X-O-X-O-W-X-X-X-X-
--X-O-O-X-X-O-O-O-X-O-O-X-O-O-X-O
-
-O-O-X-X-X-O-X-O-O-O-O-O-O-X-X-O-
--O-O-O-W-O-X-O-O-X-X-X-O-X-O-X-O
-O-O-O-O-O-O-X-X-X-X-O-X-X-X-X-X-
--X-O-O-O-O-O-O-O-X-O-X-O-O-O-X-O
-
-O-X-X-O-O-O-X-O-O-X-O-X-O-O-X-X-
--X-X-X-W-X-X-O-X-X-X-X-O-O-O-O-O
-X-O-O-O-X-O-O-X-X-O-X-O-X-X-O-O-
--X-O-X-O-X-O-O-X-O-O-O-O-X-X-O-O
-
-                """;
-
-
-        cellCount(honey);
     }
 
     public static void cellCount(String honeyComb) throws IOException {
         int count = 0;
 
 
-            for (char o : honeyComb.toCharArray()) {
-                if (o == 'O') {
-                    count++;
-                }
+        for (char o : honeyComb.toCharArray()) {
+            if (o == 'O') {
+                count++;
             }
+        }
 
 
         Files.write(Path.of("src/io/everyonecodes/java/fooling_around/Honey/level3"), String.valueOf(count).getBytes());
---
+++ src/io/everyonecodes/java/fooling_around/Honey/Round2.java
@@ -0,0 +1,84 @@
+package io.everyonecodes.java.fooling_around.Honey;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Round2 {
+
+    public static void main(String[] args) throws IOException {
+       List<String> honey = Files.readAllLines(Path.of("src/io/everyonecodes/java/fooling_around/Honey/level2_1"));
+       List<String> result = indexOfLineW(honey).stream().map(Object::toString).toList();
+
+       result.forEach(System.out::println);
+
+
+       Files.write(Path.of("src/io/everyonecodes/java/fooling_around/Honey/Level2_result"), result);
+
+
+    }
+
+
+    public static List<Integer> indexOfLineW(List<String> honey) {
+        int indexOfLine = 0;
+        List<Integer> counts = new ArrayList<>();
+
+        for (int i = 0; i < honey.size(); i++) {
+            String line = honey.get(i);
+            if (line.contains("W")) {
+                indexOfLine = i;
+                int count = count(indexOfLine, indexOfW(line), honey);
+                counts.add(count);
+            }
+        }
+
+        return counts;
+    }
+
+    public static int indexOfW(String line) {
+        int indexOfW = 0;
+
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == 'W') {
+                indexOfW = i;
+            }
+        }
+
+        return indexOfW;
+    }
+
+    public static int count(int indexOfLine, int indexOfW, List<String> honey) {
+        int count = 0;
+        String lineAbove = honey.get(indexOfLine - 1);
+        String lineBelow = honey.get(indexOfLine + 1);
+        String lineWithW = honey.get(indexOfLine);
+
+        if (lineAbove.charAt(indexOfW -1) == 'O') {
+            count ++;
+        }
+        if (lineAbove.charAt(indexOfW +1) == 'O') {
+            count ++;
+        }
+
+        if (lineWithW.charAt(indexOfW -2) == 'O') {
+            count ++;
+        }
+        if (lineWithW.charAt(indexOfW + 2) == 'O') {
+            count ++;
+        }
+
+        if (lineBelow.charAt(indexOfW -1) == 'O') {
+            count ++;
+        }
+        if (lineBelow.charAt(indexOfW + 1) == 'O') {
+            count++;
+        }
+
+        return count;
+    }
+}
